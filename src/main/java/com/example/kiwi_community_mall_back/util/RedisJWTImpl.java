0e6a820cffcb884339536c552462a909ca0d8ed0
Kiwi2333 <1329634286@qq.com>
2023-04-13 14:59:08 +0800

    添加JWT工具类

---
+++ src/main/java/com/example/kiwi_community_mall_back/util/RedisJWTImpl.java
@@ -0,0 +1,14 @@
+package com.example.kiwi_community_mall_back.util;
+
+
+/**
+ * 描述
+ *
+ * @className: RedisJWT
+ * @author: Author作者
+ * @description: TODO描述
+ * @date: 2023/4/13 14:52
+ */
+
+public final class RedisJWTImpl {
+}
